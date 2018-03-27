2608d2e71f2cd8c079a6dc2efcbced2fded89caa
ziyanwould <godisljr@163.com>
2018-03-27 17:22:12 +0800

    路由功能

--- pages/user/user.js
+++ pages/user/user.js
@@ -4,7 +4,36 @@ var app = getApp()
 Page({
   data: {
     motto: '欢迎',
-    userInfo: {}
+    userInfo: {},
+    person:"集众人之力，成细节之美！",
+    influence:"17",
+    influence_per:"20.6%",
+    jobList:[
+      {
+       "name":"投递记录",
+       "id":"0",
+       "pic": "wx_record",
+       "url":"../child/deliver/deliver",
+      },
+      {
+        "name": "职位收藏",
+        "id": "1",
+        "pic": "wx_collect",
+        "url":"../child/position/position"
+      },
+      {
+        "name": "反馈",
+        "id": "2",
+        "pic": "wx_retroaction",
+        "url":"../child/couple/couple"
+      }
+      , {
+        "name": "访客",
+        "id": "3",
+        "pic": "wx_footprint",
+        "url": "../child/visitor/visitor"
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -22,5 +51,15 @@ Page({
         userInfo:userInfo
       })
     })
-  }
+  },
+   onPullDownRefresh: function () {
+    // do somthing
+    wx.stopPullDownRefresh();
+  },
+   jumpToMyPage: function (event) {
+     
+     wx.navigateTo({
+       url: event.currentTarget.dataset.url
+     })
+   },
 })
