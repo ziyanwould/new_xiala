a142dc7f79af3b3ef585e772627e654f482fcc53
ziyanwould <godisljr@163.com>
2018-04-12 18:00:08 +0800

    公司详情页

---
+++ pages/child/CompanyPage/CompanyPage.js
@@ -0,0 +1,86 @@
+// pages/child/CompanyPage/CompanyPage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      { piCurl:"http://www.liujiarong.top/WX/pending.png"},
+      { piCurl: "http://www.liujiarong.top/WX/otherPeople.png" },
+      { piCurl: "http://www.liujiarong.top/WX/otherPeople.png" },
+      { piCurl: "http://www.liujiarong.top/WX/otherPeople.png" },
+      { piCurl: "http://www.liujiarong.top/WX/otherPeople.png" },
+      { piCurl: "http://www.liujiarong.top/WX/MoreCom.png" },
+    ],
+    list2:[
+      { address1: "广州市海珠区", address2:"中山大学科技园B座1818"},
+      { address1: "佛山市禅城区", address2: "佛山市信息科技创业园"},
+    ],
+    company:"中住71",
+    label:"50-150人/移动互联网/建筑/设计/教育",
+    attestation:"http://www.liujiarong.top/WX/certified.png",
+    types:"http://www.liujiarong.top/WX/popCompass.png",
+    companyPerson:"http://www.liujiarong.top/WX/pending.png",
+    history:"85664",
+    counturl:"http://www.liujiarong.top/WX/Comup.png"
+
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
